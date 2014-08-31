886db0a4ec9cfd8eabcc03cbe64af3e291344965
Marcin Czołnowski <marcin@czolnowski.net>
2014-08-31 17:35:23 +0200

    Add success and error.

--- src/logger.js
+++ src/logger.js
@@ -1,12 +1,32 @@
 var colors = require('colors'),
     sprintf = require('util').format;
 
-module.export.log = function ()
+module.exports.log = function ()
 {
-    process.stdout.writeln(
+    process.stdout.write(
         sprintf.apply(
             this,
             arguments
-        )
-    )
+        ) + '\n'
+    );
+};
+
+module.exports.success = function ()
+{
+    process.stdout.write(
+        sprintf.apply(
+            this,
+            arguments
+        ).green + '\n'
+    );
+};
+
+module.exports.error = function ()
+{
+    process.stdout.write(
+        sprintf.apply(
+            this,
+            arguments
+        ).red + '\n'
+    );
 };
