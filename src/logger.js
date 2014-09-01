78a0ce66925022ca82543a7c9f643a5ad3805536
Marcin Czołnowski <marcin@czolnowski.net>
2014-09-01 07:01:39 +0200

    Add verbosity levels.

--- src/logger.js
+++ src/logger.js
@@ -1,32 +1,61 @@
 var colors = require('colors'),
-    sprintf = require('util').format;
+    sprintf = require('util').format,
+    verbosityLevels = ['error', 'info', 'debug'],
+    level = 'error';
 
-module.exports.log = function ()
+var isVerbose = function isVerbose(verbose)
+    {
+        return verbosityLevels.indexOf(level) >= verbosityLevels.indexOf(verbose)
+    },
+    guessVerboseFromArguments = function guessVerboseFromArguments(args, defaultVerbose)
+    {
+        var verbose = defaultVerbose;
+
+        if (args.length > 0) {
+            var last = args[args.length - 1];
+
+            if (verbosityLevels.indexOf(last) > -1) {
+                verbose = last;
+                args.splice(-1);
+            }
+        }
+
+        return verbose;
+    };
+
+module.exports.setLevel = function (_level)
 {
-    process.stdout.write(
-        sprintf.apply(
-            this,
-            arguments
-        ) + '\n'
-    );
+    if (verbosityLevels.indexOf(_level) > -1) {
+        level = _level;
+    }
+};
+
+module.exports.info = function ()
+{
+    var args = Array.prototype.slice.call(arguments, 0),
+        verbose = guessVerboseFromArguments(args, 'info');
+
+    if (isVerbose(verbose)) {
+        process.stdout.write(sprintf.apply(this, args) + '\n');
+    }
 };
 
 module.exports.success = function ()
 {
-    process.stdout.write(
-        sprintf.apply(
-            this,
-            arguments
-        ).green + '\n'
-    );
+    var args = Array.prototype.slice.call(arguments, 0),
+        verbose = guessVerboseFromArguments(args, 'info');
+
+    if (isVerbose(verbose)) {
+        process.stdout.write(sprintf.apply(this, args).green + '\n');
+    }
 };
 
 module.exports.error = function ()
 {
-    process.stdout.write(
-        sprintf.apply(
-            this,
-            arguments
-        ).red + '\n'
-    );
+    var args = Array.prototype.slice.call(arguments, 0),
+        verbose = guessVerboseFromArguments(args, 'error');
+
+    if (isVerbose(verbose)) {
+        process.stdout.write(sprintf.apply(this, args).red + '\n');
+    }
 };
